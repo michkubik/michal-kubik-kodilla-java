504bc067608a08e24014bd4f49c6e46e616c7a81
michkubik <michal.kubik@o2.pl>
2020-08-06 16:43:40 +0200

    6.3 User tests added

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -6,33 +6,6 @@ import com.kodilla.testing.user.SimpleUser;
 public class TestingMain {
     public static void main(String[] args){
 
-        SimpleUser simpleUser = new SimpleUser("theForumUser");
-
-        String result = simpleUser.getUsername();
-
-        if (result.equals("theForumUser")){
-            System.out.println("test OK");
-        } else {
-            System.out.println("Error!");
-        }
-        // kolejny test ->
-        System.out.println("Test - pierwszy test jednostkowy:");
-
-        Calculator testCalculator = new Calculator();
-
-        int resultAdd = testCalculator.addAToB(10, 5);
-        if(resultAdd ==15) {
-            System.out.println("Add test OK");
-        } else {
-            System.out.println("Add test error!");
-        }
-
-        int resultSubtract = testCalculator.subtractAFromB(10, 5);
-        if(resultSubtract == 5) {
-            System.out.println("Subtract test OK");
-        } else {
-            System.out.println("Subtract test error!");
-        }
         }
 
 
