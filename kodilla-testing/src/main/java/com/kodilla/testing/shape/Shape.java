e359de388c67e8131d6c1944b950a21299ab0da4
michkubik <michal.kubik@o2.pl>
2020-08-13 20:21:42 +0200

    6.4. Exercise started

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/shape/Shape.java
@@ -0,0 +1,11 @@
+package com.kodilla.testing.shape;
+
+public interface Shape {
+
+    //first method in the interface
+    public String getShapeName();
+
+    //second method in the interface
+    public double getField();
+}
+}
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
@@ -0,0 +1,20 @@
+package com.kodilla.testing.shape;
+
+public class ShapeCollector {
+
+    public void addFigure(Shape shape) {
+        //empty for now
+    }
+
+    public void removeFigure(Shape shape) {
+        //empty for now
+    }
+
+    public void getFigure(int n) {
+        //empty for now
+    }
+
+    public void showFigures() {
+        //empty for now
+    }
+}
