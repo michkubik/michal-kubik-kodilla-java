2ae5d9b9f814260f5ce0ff90051b4eb55d56d235
michkubik <michal.kubik@o2.pl>
2020-09-09 10:54:25 +0200

    7.2. Stream exercise 2 - added

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.reference.FunctionalBeautifier;
 import com.kodilla.stream.reference.FunctionalCalculator;
@@ -22,5 +23,8 @@ public class StreamMain {
         System.out.println("Printing expressions with method references");
         poemBeautifier.beautify("To jest nowy wiersz", FunctionalBeautifier::addABC);
         poemBeautifier.beautify("Nana nana na na na", FunctionalBeautifier::upperCasePoem);
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
