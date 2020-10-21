ba212874273d4ce432da5831e93bd11d24c664f4
michkubik <michal.kubik@o2.pl>
2020-10-21 15:11:03 +0200

    13.1 Good Patterns - ex.1

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challanges/MovieStore.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challanges/MovieStore.java
@@ -1,4 +1,42 @@
 package com.kodilla.good.patterns.challanges;
 
-public class MovieStore {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+public final class MovieStore {
+
+    public static Map<String, List<String>> getMovies() {
+
+        final Map<String, List<String>> moviesTitlesWithTranslations = new HashMap<>();
+
+        List<String> ironManTranslations = new ArrayList<>();
+        ironManTranslations.add("Żelazny Człowiek");
+        ironManTranslations.add("Iron Man");
+
+        List<String> avengersTranslations = new ArrayList<>();
+        avengersTranslations.add("Mściciele");
+        avengersTranslations.add("Avengers");
+
+        List<String> flashTranslations = new ArrayList<>();
+        flashTranslations.add("Błyskawica");
+        flashTranslations.add("Flash");
+
+        moviesTitlesWithTranslations.put("IM", ironManTranslations);
+        moviesTitlesWithTranslations.put("AV", avengersTranslations);
+        moviesTitlesWithTranslations.put("FL", flashTranslations);
+
+        return new HashMap<>(moviesTitlesWithTranslations);
+    }
+
+    public static void main(String[] args) {
+
+        MovieStore.getMovies().entrySet().stream()
+                .flatMap(stringListEntry -> stringListEntry.getValue().stream())
+                .forEach(n -> System.out.print(n + "! "));
+    }
 }
+
+
