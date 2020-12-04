28d15ae4700aa291ac13f9b9559390c87bc170a9
michkubik <michal.kubik@o2.pl>
2020-12-04 20:00:37 +0100

    17.3 Spring i Hibernate - ex.3 - zadanie w trakcie

---
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/invoice/Item.java
@@ -0,0 +1,77 @@
+package com.kodilla.hibernate.invoice;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "ITEM")
+public class Item {
+
+    private int id;
+    private Product product;
+    private BigDecimal price;
+    private int quantity;
+    private BigDecimal value;
+
+
+    public Item() {
+    }
+
+    public Item(Product product, BigDecimal price, int quantity) {
+        this.product = product;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    @Id
+    @GeneratedValue
+    @NotNull
+    @Column(name = "ITEM_ID")
+    public int getId() {
+        return id;
+    }
+
+    private void setId(int id) {
+        this.id = id;
+    }
+
+    @NotNull
+    @Column(name = "PRODUCT")
+    public Product getProduct() {
+        return product;
+    }
+
+    private void setProduct(Product product) {
+        this.product = product;
+    }
+
+    @NotNull
+    @Column(name = "PRICE")
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    private void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    @Column(name = "QUANTITY")
+    public int getQuantity() {
+        return quantity;
+    }
+
+    private void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Column(name = "VALUE")
+    public BigDecimal getValue() {
+        value = price.multiply(new BigDecimal(quantity));
+        return value;
+    }
+
+    private void setValue(BigDecimal value) {
+        this.value = value;
+    }
+}
